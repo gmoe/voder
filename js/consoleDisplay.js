953879c483bf74a674adb35c1f196e71fa65c99b
Griffin Moe <me@griffinmoe.com>
2018-08-25 10:47:15 -0500

    Fix for impending Chrome autoplay changes.

--- js/consoleDisplay.js
+++ js/consoleDisplay.js
@@ -1,14 +1,14 @@
 "use strict";
 (function() {
 
-  const canvas = document.getElementById("consoleDisplay");
+  const canvas = document.getElementById("console-display");
   const ctx = canvas.getContext("2d");
 
   const buttonStates = {};
   function buttonState(letter) {
     if(buttonStates[letter]) {
       ctx.strokeStyle = "#ff0000";
-    } else { 
+    } else {
       ctx.strokeStyle = "#340f20";
     }
   }
@@ -22,48 +22,81 @@
     // Left Hand -------------------
     buttonState("a");
     ctx.strokeRect(2, 40, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band0' });
+    } catch(e) {}
     ctx.fillText("A", 8, 95);
 
     buttonState("s");
     ctx.strokeRect(42, 30, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band1' });
+    } catch(e) {}
     ctx.fillText("S", 52, 85);
 
     buttonState("d");
     ctx.strokeRect(82, 20, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band2' });
+    } catch(e) {}
     ctx.fillText("D", 92, 75);
 
     buttonState("f");
     ctx.strokeRect(122, 30, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band3' });
+    } catch(e) {}
     ctx.fillText("F", 134, 85);
 
     buttonState("v");
     ctx.strokeRect(162, 60, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band4' });
+    } catch(e) {}
     ctx.fillText("V", 168, 115);
 
     //Left Wrist -------------------
     buttonState(" ");
     ctx.strokeRect(42, 240, 120, 36);
+    try {
+      ctx.addHitRegion({ id: 'noise_band' });
+    } catch(e) {}
     ctx.fillText("Space", 54, 268);
 
     //Right Hand -------------------
     buttonState("b");
     ctx.strokeRect(222, 60, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band5' });
+    } catch(e) {}
     ctx.fillText("B", 234, 115);
 
     buttonState("h");
     ctx.strokeRect(262, 30, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band6' });
+    } catch(e) {}
     ctx.fillText("H", 269, 85);
 
     buttonState("j");
     ctx.strokeRect(302, 20, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band7' });
+    } catch(e) {}
     ctx.fillText("J", 312, 75);
 
     buttonState("k");
     ctx.strokeRect(342, 30, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band8' });
+    } catch(e) {}
     ctx.fillText("K", 352, 85);
 
     buttonState("l");
     ctx.strokeRect(382, 40, 36, 90);
+    try {
+      ctx.addHitRegion({ id: 'band9' });
+    } catch(e) {}
     ctx.fillText("L", 394, 95);
   }
 
--- js/voder.js
+++ js/voder.js
@@ -1,17 +1,16 @@
-"use strict";
-(function () {
+'use strict';
+document.querySelector('#start-audio-btn').addEventListener('click', function() {
 
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
   function makeFormant(f1, f2) {
-
     const sinOsc = audioCtx.createOscillator();
-    sinOsc.type = "sawtooth";
+    sinOsc.type = 'sawtooth';
     sinOsc.frequency.value = 110;
     sinOsc.start();
 
     const bandPass = audioCtx.createBiquadFilter();
-    bandPass.type = "bandpass";
+    bandPass.type = 'bandpass';
     bandPass.frequency.value = (f1+f2)/2;
     bandPass.Q.value = ((f1+f2)/2) / (f2-f1);
 
@@ -26,16 +25,16 @@
   }
 
   const formants = {};
-  formants["a"] = makeFormant(0, 225);
-  formants["s"] = makeFormant(225, 450);
-  formants["d"] = makeFormant(450, 700);
-  formants["f"] = makeFormant(700, 1000);
-  formants["v"] = makeFormant(1000, 1400);
-  formants["b"] = makeFormant(1400, 2000);
-  formants["h"] = makeFormant(2000, 2700);
-  formants["j"] = makeFormant(2700, 3800);
-  formants["k"] = makeFormant(3800, 5400);
-  formants["l"] = makeFormant(5400, 7500);
+  formants['a'] = makeFormant(0, 225);
+  formants['s'] = makeFormant(225, 450);
+  formants['d'] = makeFormant(450, 700);
+  formants['f'] = makeFormant(700, 1000);
+  formants['v'] = makeFormant(1000, 1400);
+  formants['b'] = makeFormant(1400, 2000);
+  formants['h'] = makeFormant(2000, 2700);
+  formants['j'] = makeFormant(2700, 3800);
+  formants['k'] = makeFormant(3800, 5400);
+  formants['l'] = makeFormant(5400, 7500);
 
   const noise = audioCtx.createBufferSource();
   const buffer = audioCtx.createBuffer(1, 8192, audioCtx.sampleRate);
@@ -50,14 +49,14 @@
   noise.loop = true;
 
   const noiseFilter = audioCtx.createBiquadFilter();
-  noiseFilter.type = "bandpass";
+  noiseFilter.type = 'bandpass';
   noiseFilter.frequency.value = 5000;
   noiseFilter.Q.value = 0.5;
 
   const noiseGain = audioCtx.createGain();
   noiseGain.gain.value = 0.0;
 
-  formants[" "] = noiseGain;
+  formants[' '] = noiseGain;
 
   noise.connect(noiseFilter);
   noiseFilter.connect(noiseGain);
@@ -83,5 +82,6 @@
       }
     });
   });
-  
-}());
+
+  document.querySelector('#start-audio-overlay').style = 'animation-name: disappear;';
+});
